5546ac181a1a09f1b0aa16e9dd54886bc40d2c50
Jose Garcia de Souza Junior <jose_garcia2022@outlook.com.br>
2024-06-05 10:44:19 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('form-sorteador').addEventListener('submit', function(evento) {
+        evento.preventDefault();
+        let numeroMaximo = document.getElementById('numero-maximo').value;
+        // converter o numero para inteiro.
+        numeroMaximo = parseInt(numeroMaximo);
+
+        let numeroAleatorio = Math.random() * numeroMaximo;
+        numeroAleatorio = Math.floor(numeroAleatorio + 1);
+
+        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        document.querySelector('.resultado').style.display = 'block';
+    });
+});
+
+
+
